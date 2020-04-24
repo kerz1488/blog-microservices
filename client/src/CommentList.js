d6c01521a846e624c050f6faffbb582030e2f05e
Alexis Gallardo <alexiss.gga@gmail.com>
2020-04-23 21:24:09 -0400

    Revert "basic example microservices"
    This reverts commit 5e515c2f59d51f004865829f466b3bfb50caaa38.

--- client/src/CommentList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default ({ comments }) => {
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>
-  })
-
-  return <ul>{renderedComments}</ul>
-}
