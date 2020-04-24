29127a74bb591a1cdc5d184000b63ff7448c7e9a
Alexis Gallardo <alexiss.gga@gmail.com>
2020-04-24 11:52:37 -0400

    add dockerfile

--- comments/index.js
+++ comments/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
-const commentsByPostId = {};
+const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || []);
-});
+  res.send(commentsByPostId[req.params.id] || [])
+})
 
 app.post('/posts/:id/comments', async (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
-  const { content } = req.body;
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
 
-  const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[req.params.id] || []
 
-  comments.push({ id: commentId, content, status: 'pending' });
+  comments.push({ id: commentId, content, status: 'pending' })
 
-  commentsByPostId[req.params.id] = comments;
+  commentsByPostId[req.params.id] = comments
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
@@ -32,24 +32,24 @@ app.post('/posts/:id/comments', async (req, res) => {
       postId: req.params.id,
       status: 'pending'
     }
-  });
+  })
 
-  res.status(201).send(comments);
-});
+  res.status(201).send(comments)
+})
 
 app.post('/events', async (req, res) => {
-  console.log('Event Received:', req.body.type);
+  console.log('Event Received:', req.body.type)
 
-  const { type, data } = req.body;
+  const { type, data } = req.body
 
   if (type === 'CommentModerated') {
-    const { postId, id, status, content } = data;
-    const comments = commentsByPostId[postId];
+    const { postId, id, status, content } = data
+    const comments = commentsByPostId[postId]
 
     const comment = comments.find(comment => {
-      return comment.id === id;
-    });
-    comment.status = status;
+      return comment.id === id
+    })
+    comment.status = status
 
     await axios.post('http://localhost:4005/events', {
       type: 'CommentUpdated',
@@ -59,12 +59,12 @@ app.post('/events', async (req, res) => {
         postId,
         content
       }
-    });
+    })
   }
 
-  res.send({});
-});
+  res.send({})
+})
 
 app.listen(4001, () => {
-  console.log('Listening on 4001');
-});
+  console.log('Listening on 4001')
+})
--- moderation/index.js
+++ moderation/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
 app.post('/events', async (req, res) => {
-  const { type, data } = req.body;
+  const { type, data } = req.body
 
   if (type === 'CommentCreated') {
-    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    const status = data.content.includes('orange') ? 'rejected' : 'approved'
 
     await axios.post('http://localhost:4005/events', {
       type: 'CommentModerated',
@@ -19,12 +19,12 @@ app.post('/events', async (req, res) => {
         status,
         content: data.content
       }
-    });
+    })
   }
 
-  res.send({});
-});
+  res.send({})
+})
 
 app.listen(4003, () => {
-  console.log('Listening on 4003');
-});
+  console.log('Listening on 4003')
+})
